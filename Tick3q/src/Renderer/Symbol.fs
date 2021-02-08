7b2c343da17963d2ade3b14ecdfddc2ae4a6fd97
SaraFFdez <SaraFFdez@users.noreply.github.com>
2021-02-08 18:11:03 +0100

    Printing fixed

--- Tick3q/src/Renderer/Symbol.fs
+++ Tick3q/src/Renderer/Symbol.fs
@@ -23,6 +23,11 @@ let posOf x y = {X=x;Y=y} // helper
 // add your own functions as needed
 //Gotta change this!!!!!
 let isaComponent = true  
+
+let decComp comp = 
+    match comp.Type with
+    |BusDecoder (w,a,n) -> (w,a,n)
+    |_ -> failwithf "not a valid component"
     
 //-----------------------------------------------------------------------------------------//
 
@@ -31,15 +36,10 @@ let isaComponent = true
 /// you may add to type definition in CommonTypes
 let makeBusDecoderComponent (pos:XYPos) (w: int) (a: int) (n: int) = 
     let comp =  {
-        (*Type = BusDecoder (w,a,n);
-        X = int pos.X;
-        Y = int pos.Y;
-        H = 17+10*n;
-        W = 60;*)
         X = int pos.X
         Y = int pos.Y
         W = 60 
-        H = 17+(10*n) 
+        H = 17+(11*n) 
         Type = BusDecoder (w,a,n)
     }
     comp
@@ -84,6 +84,57 @@ let busDecoderView (comp: Component) =
             //
             let scaleFactor=1.0 // to demonstrate svg scaling
             let rotation=0 // to demonstrate svg rotation (in degrees)
+            let lst = [a..(a+n-1)]
+            let stringLst = List.map (fun x -> (string(x)) ) lst
+            let indexedStrLst = List.indexed stringLst
+            let transToElem (i,x) = 
+                let value = float(30+10*i)
+                text [ // a demo text svg element
+                    X 20.; 
+                    Y value; 
+                    Style [
+                        TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                        DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                        FontSize "6px"
+                        FontWeight "Bold"
+                        Fill "Black" // demo font color
+                    ]
+                ] [str <| sprintf "%A" x]
+            let printNumbers = List.map (transToElem) indexedStrLst
+            let allElement = 
+                        polygon [ // a demo svg polygon quad
+                            SVGAttr.Points (sprintf "-30,10 %i,10 %i,%i -30,%i" (-30+comp.W) (-30+comp.W) (10+comp.H) (10+comp.H)) // "--30,10 -30+comp.W,10 -30+comp.W,10+comp.H -30,10+comp.H"
+                            SVGAttr.StrokeWidth "2px"
+                            SVGAttr.Stroke "Black"
+                            SVGAttr.FillOpacity 0.1
+                            SVGAttr.Fill "Gray"] []::
+        
+                        text [ // a demo text svg element
+                            X 0.; 
+                            Y 20.; 
+                            Style [
+                                TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                                DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                                FontSize "6px"
+                                FontWeight "Bold"
+                                Fill "Black" // demo font color
+                            ]
+                        ] [str <| sprintf "Bus Decode"]::
+        
+        
+        
+                        text [ // a demo text svg element
+                            X -20.; 
+                            Y 70.; 
+                            Style [
+                                TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                                DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                                FontSize "6px"
+                                FontWeight "Bold"
+                                Fill "Black" // demo font color
+                            ]
+                        ] [str <| sprintf "In"]::
+                        printNumbers
             g   [ Style [ 
                     // the transform here does rotation, scaling, and translation
                     // the rotation and scaling happens with TransformOrigin as fixed point first
@@ -94,50 +145,7 @@ let busDecoderView (comp: Component) =
                 ]  // g optional attributes in first list
                 // use g svg element (SVG equivalent of div) to group any number of ReactElements into one.
                 // use transform with scale and/or translate and/or rotate to transform group
-                [
-                    polygon [ // a demo svg polygon quad
-                        SVGAttr.Points (sprintf "-30,10 %i,10 %i,%i -30,%i" (-30+comp.W) (-30+comp.W) (10+comp.H) (10+comp.H)) // "-30,10 -30+comp.W,10 -30+comp.W,10+comp.H -30,10+comp.H"
-                        SVGAttr.StrokeWidth "2px"
-                        SVGAttr.Stroke "Black"
-                        SVGAttr.FillOpacity 0.1
-                        SVGAttr.Fill "Gray"] []
-                    text [ // a demo text svg element
-                        X 0.; 
-                        Y 20.; 
-                        Style [
-                            TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                            DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                            FontSize "6px"
-                            FontWeight "Bold"
-                            Fill "Black" // demo font color
-                        ]
-                    ] [str <| sprintf "Bus Decode"]
-                    text [ // a demo text svg element
-                        X -20.; 
-                        Y 70.; 
-                        Style [
-                            TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                            DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                            FontSize "6px"
-                            FontWeight "Bold"
-                            Fill "Black" // demo font color
-                        ]
-                    ] [str <| sprintf "In"]
-                    let lst = [a..(a+n-1)]
-                    let stringLst = List.map (fun x -> string(x)) lst
-                    let printableS = stringLst |> List.fold (fun y x -> y + x + "\n") ""
-                    text [ 
-                        X 20.; 
-                        Y 70.; 
-                        Style [
-                            TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                            DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                            FontSize "6px"
-                            FontWeight "Bold"
-                            Fill "Black" // demo font color
-                        ]
-                    ] [str <| printableS] 
-                ]      
+                allElement    
     else failwithf "The element provided is not a Component and cannot be viewed"
 
 /// demo function can be deleted
@@ -155,6 +163,58 @@ let busDecoderViewDummy (comp: Component) =
     //
     let scaleFactor=1.0 // to demonstrate svg scaling
     let rotation=0 // to demonstrate svg rotation (in degrees)
+    let lst = [a..(a+n-1)]
+    let stringLst = List.map (fun x -> (string(x)) ) lst
+    let indexedStrLst = List.indexed stringLst
+    let transToElem (i,x) = 
+        let value = float(10+10*i)
+        text [ // a demo text svg element
+            X 20.; 
+            Y value; 
+            Style [
+                TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                FontSize "6px"
+                FontWeight "Bold"
+                Fill "Black" // demo font color
+            ]
+        ] [str <| sprintf "%A" x]
+    let printNumbers = List.map (transToElem) indexedStrLst
+    let utkufunc = 
+                polygon [ // a demo svg polygon quad
+                    SVGAttr.Points "-30,10 30,10 30,120 -30,120" // "--30,10 -30+comp.W,10 -30+comp.W,10+comp.H -30,10+comp.H"
+                    SVGAttr.StrokeWidth "2px"
+                    SVGAttr.Stroke "Black"
+                    SVGAttr.FillOpacity 0.1
+                    SVGAttr.Fill "Gray"] []::
+
+                text [ // a demo text svg element
+                    X 0.; 
+                    Y 20.; 
+                    Style [
+                        TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                        DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                        FontSize "6px"
+                        FontWeight "Bold"
+                        Fill "Black" // demo font color
+                    ]
+                ] [str <| sprintf "Bus Decode"]::
+
+
+
+                text [ // a demo text svg element
+                    X -20.; 
+                    Y 70.; 
+                    Style [
+                        TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
+                        DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
+                        FontSize "6px"
+                        FontWeight "Bold"
+                        Fill "Black" // demo font color
+                    ]
+                ] [str <| sprintf "In"]::
+                printNumbers
+
     g   [ Style [ 
             // the transform here does rotation, scaling, and translation
             // the rotation and scaling happens with TransformOrigin as fixed point first
@@ -165,60 +225,16 @@ let busDecoderViewDummy (comp: Component) =
         ]  // g optional attributes in first list
         // use g svg element (SVG equivalent of div) to group any number of ReactElements into one.
         // use transform with scale and/or translate and/or rotate to transform group
-        [
-            polygon [ // a demo svg polygon quad
-                SVGAttr.Points "-30,10 30,10 30,120 -30,120" // "--30,10 -30+comp.W,10 -30+comp.W,10+comp.H -30,10+comp.H"
-                SVGAttr.StrokeWidth "2px"
-                SVGAttr.Stroke "Black"
-                SVGAttr.FillOpacity 0.1
-                SVGAttr.Fill "Gray"] []
-            text [ // a demo text svg element
-                X 0.; 
-                Y 20.; 
-                Style [
-                    TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                    DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                    FontSize "6px"
-                    FontWeight "Bold"
-                    Fill "Black" // demo font color
-                ]
-            ] [str <| sprintf "Bus Decode"]
-            text [ // a demo text svg element
-                X -20.; 
-                Y 70.; 
-                Style [
-                    TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                    DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                    FontSize "6px"
-                    FontWeight "Bold"
-                    Fill "Black" // demo font color
-                ]
-            ] [str <| sprintf "In"]
-            let lst = [a..(a+n-1)]
-            let stringLst = List.map (fun x -> string(x)) lst
-            let printableS = stringLst |> List.fold (fun y x -> y + x + "\n") ""
-            text [ 
-                X 20.; 
-                Y 70.; 
-                Style [
-                    TextAnchor "middle" // left/right/middle: horizontal algnment vs (X,Y)
-                    DominantBaseline "hanging" // auto/middle/hanging: vertical alignment vs (X,Y)
-                    FontSize "6px"
-                    FontWeight "Bold"
-                    Fill "Black" // demo font color
-                ]
-            ] [str <| printableS]
-            //[str <| sprintf "X=%.0f Y=%.0f" fX fY]
-            // child of text element is text to display
-    ]
+        //[
+        utkufunc
        
 
 
 /// View function - in this case view is independent of model
 let view (model : Model) (dispatch : Msg -> unit) =    
     [   // change for Tick3 answer
-        makeBusDecoderComponent {X=70.; Y=20.} 69 7 5// for Tick 3 two components
-        makeBusDecoderComponent {X=200.; Y=20.} 69 7 8
+        makeBusDecoderComponent {X=70.; Y=20.} 69 7 8// for Tick 3 two components
+        makeBusDecoderComponent {X=200.; Y=20.} 69 7 5
     ] 
     |> List.map busDecoderView // change for Tick3 answer
     |> (fun svgEls -> 
